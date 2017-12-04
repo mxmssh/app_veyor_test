b802cdaff1224a7f4db5e0d51d7d434f082e227a
unknown <mxmssh@gmail.com>
2017-12-04 21:53:18 +0300

    initial test commit

---
+++ app_veyor_test.cpp
@@ -0,0 +1,12 @@
+// app_veyor_test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hellow AppVeyor\n");
+	return 0;
+}
+
